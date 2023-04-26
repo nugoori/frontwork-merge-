ef82c18d22f126f65e1dcc0fbb07b04ef2134e15
cheolmin99 <cjfals8570@naver.com>
2023-04-26 14:43:33 +0900

    아이디, 닉네임 중복체크 완료

--- board/src/main/java/com/example/demo/service/implementation/UserServiceImplements.java
+++ board/src/main/java/com/example/demo/service/implementation/UserServiceImplements.java
@@ -3,8 +3,12 @@ package com.example.demo.service.implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.request.user.ValidateEmailDto;
+import com.example.demo.dto.request.user.ValidateNicknameDto;
 import com.example.demo.dto.response.ResponseDto;
 import com.example.demo.dto.response.user.GetUserResponseDto;
+import com.example.demo.dto.response.user.ValidateEmailResponseDto;
+import com.example.demo.dto.response.user.ValidateNicknameResponseDto;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
@@ -13,9 +17,11 @@ import com.example.demo.common.constant.ResponseMessage;
 @Service //서비스로 지정
 public class UserServiceImplements implements UserService {
     
-    @Autowired private UserRepository userRepository;
+    @Autowired 
+    private UserRepository userRepository;
 
     public ResponseDto<GetUserResponseDto> getUser(String email) {
+
         GetUserResponseDto data = null;
 
         // 찾는 값이 있으면 성공 메세지를 반환, 데이터 베이스가 오류거나, 찾는 값이 없으면 에러메세지 반환.
@@ -30,4 +36,38 @@ public class UserServiceImplements implements UserService {
         }
         return ResponseDto.setSuccess(ResponseMessage.SUCCESS, data);
     }
+
+    public ResponseDto<ValidateEmailResponseDto> validateEmail(ValidateEmailDto dto) {
+        ValidateEmailResponseDto data = null;
+
+        String email = dto.getEmail();
+
+        try {
+            boolean hasEmail = userRepository.existsByEmail(email);
+            data = new ValidateEmailResponseDto(!hasEmail);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.setFailed(ResponseMessage.DATABASE_ERROR);
+        }
+
+        return ResponseDto.setSuccess(ResponseMessage.SUCCESS, data);
+    }
+
+    public ResponseDto<ValidateNicknameResponseDto> validateNickname(ValidateNicknameDto dto) {
+        ValidateNicknameResponseDto data = null;
+
+        String nickname = dto.getNickname();
+
+        try {
+            boolean hasNickname = userRepository.existsByNickname(nickname);
+            data = new ValidateNicknameResponseDto(!hasNickname);
+        }catch (Exception exception){
+            exception.printStackTrace();
+            return ResponseDto.setFailed(ResponseMessage.DATABASE_ERROR);
+        }
+        
+        return ResponseDto.setSuccess(ResponseMessage.SUCCESS, data);
+    }
+
 }
+
