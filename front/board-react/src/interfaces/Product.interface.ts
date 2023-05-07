be34306fc5c81f4b8ce723f444d8b6e1a3be4146
cheolmin99 <cjfals8570@naver.com>
2023-05-08 01:31:31 +0900

    호진이형이 작성한 폴더 갖고옴 / interface, mock

---
+++ front/board-react/src/interfaces/Product.interface.ts
@@ -0,0 +1,8 @@
+interface Product {
+    productNumber : number;
+    productName : string;
+    productPrice : number;
+    productUrl : string;
+    productImgUrl : string; //? 굳이 필요한가????
+}
+export default Product;
